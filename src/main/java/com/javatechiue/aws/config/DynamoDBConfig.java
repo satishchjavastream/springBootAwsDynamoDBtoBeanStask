c766c48d8e8969be09db6f827c8df14a50f986ee
Satish Chitimoju <satishchjavastream@gmail.com>
2023-08-16 17:40:16 -0500

    spring boot java 17 application with DynamoDB (.war) deployed into AWS Beanstack

---
+++ src/main/java/com/javatechiue/aws/config/DynamoDBConfig.java
@@ -0,0 +1,30 @@
+package com.javatechiue.aws.config;
+
+import com.amazonaws.auth.AWSStaticCredentialsProvider;
+import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.client.builder.AwsClientBuilder;
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDB;
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClientBuilder;
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class DynamoDBConfig {
+
+    public static final String SERVICE_ENDPOINT = "dynamodb.us-east-1.amazonaws.com";
+    public static final String REGION = "us-east-1";
+    public static final String ACCESS_KEY = "AKIAVRNYCWTHZXLWS2M4";
+    public static final String SECRET_KEY = "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";
+
+    @Bean
+    public DynamoDBMapper mapper() {
+        return new DynamoDBMapper(amazonDynamoDBConfig());
+    }
+
+    private AmazonDynamoDB amazonDynamoDBConfig() {
+        return AmazonDynamoDBClientBuilder.standard()
+                .withEndpointConfiguration(new AwsClientBuilder.EndpointConfiguration(SERVICE_ENDPOINT, REGION))
+                .withCredentials(new AWSStaticCredentialsProvider(new BasicAWSCredentials(ACCESS_KEY, SECRET_KEY))).build();
+    }
+}
